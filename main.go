f22900f09e1c483455922470c1a987e3a782125a
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-07-14 14:00:44 +0700

    graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/dotenv-org/godotenvvault"
 	"github.com/labstack/echo/v4"
@@ -12,15 +16,35 @@ import (
 )
 
 func main() {
+	// setup .env
 	err := godotenvvault.Load()
 	if err != nil {
 		log.Fatal("error reading .env")
 	}
 
 	e := echo.New()
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+	defer stop()
+
+	go startServer(e)
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
+
+func startServer(e *echo.Echo) {
 	e.Use(middleware.Logger())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, fmt.Sprintf("%s, World!", os.Getenv("HELLO")))
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+
+	if err := e.Start(":8080"); err != nil && err != http.ErrServerClosed {
+		e.Logger.Fatal("shutting down the server")
+	}
 }
