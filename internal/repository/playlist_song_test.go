5a3fa2936536c3a71772dcab53cab5faf43d0190
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-07-29 22:48:37 +0700

    extract test container setup

--- internal/repository/playlist_song_test.go
+++ internal/repository/playlist_song_test.go
@@ -2,53 +2,13 @@ package repository
 
 import (
 	"context"
-	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
 	"github.com/tuannamnguyen/playlist-manager/internal/model"
 )
-
-func setupTestDB(t *testing.T, initScriptPath string) (*sqlx.DB, func()) {
-	var (
-		dbUser     = "postgres"
-		dbPassword = "password"
-	)
-	ctx := context.Background()
-
-	postgresContainer, err := postgres.Run(ctx,
-		"postgres:latest",
-		postgres.WithInitScripts(filepath.Join(".", "testdata", initScriptPath)),
-		postgres.WithUsername(dbUser),
-		postgres.WithPassword(dbPassword),
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("database system is ready to accept connections").
-				WithOccurrence(2).
-				WithStartupTimeout(5*time.Second)),
-	)
-	require.NoError(t, err, "failed to start container")
-
-	connectionString, err := postgresContainer.ConnectionString(ctx, "dbname=playlist_manager")
-	require.NoError(t, err, "failed to get connection string")
-
-	db, err := sqlx.Connect("pgx", connectionString)
-	require.NoError(t, err, "Unable to connect to database")
-
-	cleanup := func() {
-		db.Close()
-		if err := postgresContainer.Terminate(ctx); err != nil {
-			t.Logf("failed to terminate container: %s", err)
-		}
-	}
-
-	return db, cleanup
-}
 
 func TestPlaylistSongRepositoryInsert(t *testing.T) {
 	db, cleanup := setupTestDB(t, "script_test_insert_playlist_song.sql")
--- internal/repository/song_test.go
+++ internal/repository/song_test.go
@@ -2,85 +2,54 @@ package repository
 
 import (
 	"context"
-	"log"
-	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
 	"github.com/tuannamnguyen/playlist-manager/internal/model"
 )
 
 func TestSelectWithManyID(t *testing.T) {
-	ctx := context.Background()
-	var (
-		dbUser     = "postgres"
-		dbPassword = "password"
-	)
+	db, cleanup := setupTestDB(t, "script_test_get_all_song.sql")
+	defer cleanup()
 
-	postgresContainer, err := postgres.Run(ctx,
-		"postgres:latest",
-		postgres.WithInitScripts(filepath.Join(".", "testdata", "script_test_get_all_song.sql")),
-		postgres.WithUsername(dbUser),
-		postgres.WithPassword(dbPassword),
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("database system is ready to accept connections").
-				WithOccurrence(2).
-				WithStartupTimeout(5*time.Second)),
-	)
-	if err != nil {
-		log.Fatalf("failed to start container: %s", err)
-	}
-
-	defer func() {
-		if err := postgresContainer.Terminate(ctx); err != nil {
-			log.Fatalf("failed to terminate container: %s", err)
-		}
-	}()
-
-	connectionString, err := postgresContainer.ConnectionString(ctx, "dbname=playlist_manager")
-	if err != nil {
-		log.Fatalf("failed to get connection string: %s", err)
-	}
-
-	// setup DB
-
-	db, err := sqlx.Connect("pgx", connectionString)
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
-	}
-	defer db.Close()
-
-	t.Run("success get all songs", func(t *testing.T) {
-		songRepository := NewSongRepository(db)
-		songsDetail, err := songRepository.SelectWithManyID(context.Background(), []string{"asiuasubfasuifaufb"})
-		require.NoError(t, err)
-
-		parsedUpdatedAt, err := time.Parse(time.DateTime, "2024-07-27 10:12:00")
-		require.NoError(t, err)
-
-		parsedCreatedAt, err := time.Parse(time.DateTime, "2024-07-27 10:12:00")
-		require.NoError(t, err)
-
-		expectedSongs := []model.Song{
-			{
-				ID:       "asiuasubfasuifaufb",
-				Name:     "devil in a new dress",
-				ArtistID: "kanye west",
-				AlbumID:  "mbdtf",
-				Timestamp: model.Timestamp{
-					UpdatedAt: parsedCreatedAt,
-					CreatedAt: parsedUpdatedAt,
+	tests := []struct {
+		name    string
+		songIDs []string
+		want    []model.Song
+		wantErr bool
+	}{
+		{
+			name:    "success get all songs",
+			songIDs: []string{"asiuasubfasuifaufb"},
+			want: []model.Song{
+				{
+					ID:       "asiuasubfasuifaufb",
+					Name:     "devil in a new dress",
+					ArtistID: "kanye west",
+					AlbumID:  "mbdtf",
+					Timestamp: model.Timestamp{
+						UpdatedAt: time.Date(2024, 7, 27, 10, 12, 0, 0, time.UTC),
+						CreatedAt: time.Date(2024, 7, 27, 10, 12, 0, 0, time.UTC),
+					},
 				},
 			},
-		}
+			wantErr: false,
+		},
+		// Add more test cases here if needed
+	}
 
-		assert.NoError(t, err)
-		assert.Equal(t, expectedSongs, songsDetail)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			songRepository := NewSongRepository(db)
+			got, err := songRepository.SelectWithManyID(context.Background(), tt.songIDs)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
 }
