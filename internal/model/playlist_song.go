d4d0d3e8f01059031353bad9fb383e57b776da09
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-07-31 22:02:07 +0700

    update id types

--- internal/model/playlist_song.go
+++ internal/model/playlist_song.go
@@ -1,7 +1,7 @@
 package model
 
 type PlaylistSong struct {
-	PlaylistID string `json:"playlist_id" db:"playlist_id"`
-	SongID     string `json:"song_id" db:"song_id"`
+	PlaylistID int `json:"playlist_id" db:"playlist_id"`
+	SongID     int `json:"song_id" db:"song_id"`
 	Timestamp
 }
