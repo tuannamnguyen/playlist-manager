a846e4fdd1557e0f21e0b99887b9693d47630b05
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-09-25 21:59:18 +0700

    init terraform

---
+++ infra/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.4.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "playlist-manager-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = "playlist-manager-430015"
+}
