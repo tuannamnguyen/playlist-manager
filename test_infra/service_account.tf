b7ffc0517a4ab89c3ba1e9885b1d5624da39576b
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-10-07 12:37:08 +0700

    update iam binding

--- test_infra/service_account.tf
+++ test_infra/service_account.tf
@@ -3,3 +3,9 @@ resource "google_service_account" "backend_test_service_account" {
   display_name                 = "playist-manager-backend-test"
   create_ignore_already_exists = true
 }
+
+resource "google_service_account_iam_binding" "iam_binding_token_creator" {
+  service_account_id = google_service_account.backend_test_service_account.id
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = ["serviceAccount:${google_service_account.backend_test_service_account.email}"]
+}
