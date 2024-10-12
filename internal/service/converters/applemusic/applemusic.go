14d65d19432a554177d517793d83df95acb1578e
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-10-12 10:51:50 +0700

    draft search and convert

--- internal/service/converters/applemusic/applemusic.go
+++ internal/service/converters/applemusic/applemusic.go
@@ -2,6 +2,7 @@ package applemusicconverter
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	applemusic "github.com/minchao/go-apple-music"
@@ -23,7 +24,12 @@ func New(ctx context.Context, musicUserToken string) *AppleMusicConverter {
 }
 
 func (a *AppleMusicConverter) Export(ctx context.Context, playlistName string, songs []model.SongOutAPI) error {
-	_, _, err := a.client.Me.CreateLibraryPlaylist(
+	libraryPlaylistTracks, err := a.parseSongToPlaylistTracks(ctx, songs)
+	if err != nil {
+		return fmt.Errorf("parse song models to playlist tracks: %w", err)
+	}
+
+	_, _, err = a.client.Me.CreateLibraryPlaylist(
 		ctx,
 		applemusic.CreateLibraryPlaylist{
 			Attributes: applemusic.CreateLibraryPlaylistAttributes{
@@ -32,11 +38,48 @@ func (a *AppleMusicConverter) Export(ctx context.Context, playlistName string, s
 			},
 			Relationships: &applemusic.CreateLibraryPlaylistRelationships{
 				Tracks: applemusic.CreateLibraryPlaylistTrackData{
-					Data: []applemusic.CreateLibraryPlaylistTrack{},
+					Data: libraryPlaylistTracks,
 				},
 			},
 		},
 		&applemusic.Options{},
 	)
-	return err
+
+	if err != nil {
+		return fmt.Errorf("create apple music playlist: %w", err)
+	}
+
+	return nil
+}
+
+func (a *AppleMusicConverter) parseSongToPlaylistTracks(ctx context.Context, songs []model.SongOutAPI) ([]applemusic.CreateLibraryPlaylistTrack, error) {
+	libraryPlaylistTracks := make([]applemusic.CreateLibraryPlaylistTrack, len(songs))
+
+	for i, song := range songs {
+		amTrack, err := a.searchAndMatch(ctx, song)
+		if err != nil {
+			return nil, fmt.Errorf("search and match apple music track: %w", err)
+		}
+
+		libraryPlaylistTracks[i] = amTrack
+	}
+
+	return libraryPlaylistTracks, nil
+}
+
+func (a *AppleMusicConverter) searchAndMatch(ctx context.Context, song model.SongOutAPI) (applemusic.CreateLibraryPlaylistTrack, error) {
+	amTrack, _, err := a.client.Catalog.GetSongsByIsrcs(
+		ctx,
+		"vn",
+		[]string{song.ISRC},
+		&applemusic.Options{},
+	)
+	if err != nil {
+		return applemusic.CreateLibraryPlaylistTrack{}, err
+	}
+
+	return applemusic.CreateLibraryPlaylistTrack{
+		Id:   amTrack.Data[0].Id,
+		Type: amTrack.Data[0].Type,
+	}, nil
 }
