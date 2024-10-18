8727e8bb6e9a7490c3ba5f1e785aa7167a85f5ea
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-10-19 00:56:38 +0700

    cloud storage for prod

---
+++ terraform/prod/service_account.tf
@@ -0,0 +1,18 @@
+resource "google_service_account" "backend_prod_service_account" {
+  account_id                   = "playist-manager-backend-test"
+  display_name                 = "playist-manager-backend-test"
+  create_ignore_already_exists = true
+}
+
+
+resource "google_project_iam_member" "token_creator_iam_prod" {
+  project = "playlist-manager-437214"
+  member  = "serviceAccount:${google_service_account.backend_prod_service_account.email}"
+  role    = "roles/iam.serviceAccountTokenCreator"
+}
+
+resource "google_project_iam_member" "storage_object_user_iam" {
+  project = "playlist-manager-437214"
+  member  = "serviceAccount:${google_service_account.backend_prod_service_account.email}"
+  role    = "roles/storage.objectUser"
+}
