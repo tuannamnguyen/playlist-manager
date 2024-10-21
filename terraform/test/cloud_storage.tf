c157c77cc55dcf14841e4dbee8bef3bd67e98c99
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-10-21 22:55:03 +0700

    update tf config bucket test

--- terraform/test/cloud_storage.tf
+++ terraform/test/cloud_storage.tf
@@ -3,5 +3,6 @@ resource "google_storage_bucket" "backend_test_bucket" {
   location      = "ASIA-SOUTHEAST1"
   storage_class = "STANDARD"
 
+  force_destroy               = false
   uniform_bucket_level_access = true
 }
