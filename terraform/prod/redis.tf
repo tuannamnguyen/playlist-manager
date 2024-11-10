e4f6ae29a2cd909fcdbe4761e1862b924e695cdf
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-11-10 23:40:17 +0700

    vpc config

--- terraform/prod/redis.tf
+++ terraform/prod/redis.tf
@@ -1,7 +1,8 @@
 resource "google_redis_instance" "playlist_manager_redis" {
-  name           = "playlist-manager-prod-redis"
-  tier           = "BASIC"
-  memory_size_gb = "2"
-  region         = local.region
-  redis_version  = "REDIS_7_0"
+  name               = "playlist-manager-prod-redis"
+  tier               = "BASIC"
+  memory_size_gb     = "2"
+  region             = local.region
+  redis_version      = "REDIS_7_0"
+  authorized_network = google_compute_network.backend_vpc_network.id
 }
