2efaf3bb61d82b9fa8a5a368c6c58d07a2592761
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-11-25 23:38:43 +0700

    update migrations

---
+++ scripts/migrations/000001_init_db.down.sql
@@ -0,0 +1,20 @@
+-- Drop triggers first to avoid issues with dependent tables
+DROP TRIGGER IF EXISTS set_timestamp_artist_song ON artist_song;
+DROP TRIGGER IF EXISTS set_timestamp_playlist_song ON playlist_song;
+DROP TRIGGER IF EXISTS set_timestamp_song ON song;
+DROP TRIGGER IF EXISTS set_timestamp_artist_album ON artist_album;
+DROP TRIGGER IF EXISTS set_timestamp_artist ON artist;
+DROP TRIGGER IF EXISTS set_timestamp_album ON album;
+DROP TRIGGER IF EXISTS set_timestamp_playlist ON playlist;
+
+-- Drop tables in reverse order of creation
+DROP TABLE IF EXISTS artist_song;
+DROP TABLE IF EXISTS playlist_song;
+DROP TABLE IF EXISTS song;
+DROP TABLE IF EXISTS artist_album;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS playlist;
+
+-- Drop the function last as it might be used by triggers
+DROP FUNCTION IF EXISTS update_updated_at_column;
