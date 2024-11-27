965c0d50ed36d5e9bc693ca84119e569900ad4b9
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-11-27 23:35:31 +0700

    update postgres grant privileges config

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -9,6 +9,11 @@ terraform {
       source  = "hashicorp/random"
       version = "3.6.3"
     }
+
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.24.0"
+    }
   }
 
   backend "gcs" {
@@ -20,6 +25,15 @@ terraform {
 provider "google" {
   project = local.project_id
   region  = local.region
+}
+
+provider "postgresql" {
+  host     = google_sql_database_instance.prod_db.public_ip_address
+  port     = 5432
+  database = "postgres"
+  username = "postgres_user"
+  password = var.db_root_password
+  sslmode  = "require"
 }
 
 locals {
