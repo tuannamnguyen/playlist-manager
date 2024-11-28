4f4085c9666068f7d8e439999c74a5f8c6e78a72
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-11-28 23:00:42 +0700

    update env and postgres config

--- terraform/postgres/postgres_config.tf
+++ terraform/postgres/postgres_config.tf
@@ -1,7 +1,24 @@
-resource "postgresql_grant" "all_privileges" {
-  database    = "playlist_manager"
-  role        = "playlist_manager_be"
-  privileges  = ["SELECT", "INSERT", "UPDATE", "DELETE"]
+resource "postgresql_grant" "table_privileges" {
+  database = "playlist_manager"
+  role     = "playlist_manager_be"
+  privileges = [
+    "SELECT",
+    "INSERT",
+    "UPDATE",
+    "DELETE"
+  ]
   object_type = "table"
+  schema      = "public"
+}
+
+resource "postgresql_grant" "schema_privileges" {
+  database = "playlist_manager"
+  role     = "playlist_manager_be"
+  privileges = [
+    "SELECT",
+    "USAGE",
+    "UPDATE"
+  ]
+  object_type = "sequence"
   schema      = "public"
 }
