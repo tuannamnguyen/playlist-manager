3d9ba69b36c64405090f7cdac47acc6b0f8e611c
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-12-05 00:28:29 +0700

    add vm to connect to redis

--- terraform/prod/redis.tf
+++ terraform/prod/redis.tf
@@ -6,3 +6,21 @@ resource "google_redis_instance" "playlist_manager_redis" {
   redis_version      = "REDIS_7_0"
   authorized_network = google_compute_network.backend_vpc_network.id
 }
+
+# VM to port forward Redis for local connection
+resource "google_compute_instance" "redis_connection_instance" {
+  name         = "redis-connect"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+  }
+}
