ec6db563f43ad38061675936649ac6a76cedd436
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-01-03 04:30:40 +0000

    update terraform config

--- terraform/aws_prod/main.tf
+++ terraform/aws_prod/main.tf
@@ -7,9 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "tf-state-533267191229"
-    key    = "tf-state"
-    region = "ap-southeast-1"
+    bucket  = "tf-state-533267191229"
+    key     = "tf-state"
+    region  = "ap-southeast-1"
+    profile = "admin-access"
   }
 }
 
