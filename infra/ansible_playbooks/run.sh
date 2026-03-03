6b8b3beb6f52aed5f4de5137e05c05fcd214f26c
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-03-03 14:55:44 +0000

    update script

--- infra/ansible_playbooks/run.sh
+++ infra/ansible_playbooks/run.sh
@@ -2,4 +2,5 @@ target_ip=$(terraform -chdir=../aws_prod  output -raw my_host_01_public_ip)
 # echo $target_ip
 
 
-ansible-playbook -i inventory.yaml  install-docker.yaml -e "target_ip=$target_ip ssh_private_key_path=/home/nam/.ssh/playlist_manager"
+
+ansible-playbook -i inventory.yaml  deploy.yaml -e "target_ip=$target_ip ssh_private_key_path=/home/nam/.ssh/playlist_manager"
