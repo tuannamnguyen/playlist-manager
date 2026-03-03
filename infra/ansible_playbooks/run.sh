844e5e75c613d44798bf1e310b446427f3c3956d
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-03-03 15:14:06 +0000

    split ansible playbook

--- infra/ansible_playbooks/run.sh
+++ infra/ansible_playbooks/run.sh
@@ -3,4 +3,4 @@ target_ip=$(terraform -chdir=../aws_prod  output -raw my_host_01_public_ip)
 
 
 
-ansible-playbook -i inventory.yaml  deploy.yaml -e "target_ip=$target_ip ssh_private_key_path=/home/nam/.ssh/playlist_manager"
+ansible-playbook -i inventory.yaml  main.yaml -e "target_ip=$target_ip ssh_private_key_path=/home/nam/.ssh/playlist_manager"
