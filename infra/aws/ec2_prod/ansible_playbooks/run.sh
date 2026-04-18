9b5f55bfb46bf6459ea0568e64389ac7a6716bfb
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-04-18 15:37:29 +0000

    add dotenvx private key

--- infra/aws/ec2_prod/ansible_playbooks/run.sh
+++ infra/aws/ec2_prod/ansible_playbooks/run.sh
@@ -3,6 +3,7 @@ ansible_ssm_bucket_name=$(terraform -chdir=../  output -raw ansible_ssm_bucket_n
 echo $instance_id
 echo $ansible_ssm_bucket_name
 
+read -r -s -p "Enter dotenvx private key: " dotenvx_private_key
+echo ""
 
-
-ansible-playbook -v -i inventory.yaml  main.yaml -e "instance_id=$instance_id ansible_ssm_bucket_name=$ansible_ssm_bucket_name"
+ansible-playbook -v -i inventory.yaml  main.yaml -e "instance_id=$instance_id ansible_ssm_bucket_name=$ansible_ssm_bucket_name dotenvx_private_key=$dotenvx_private_key"
